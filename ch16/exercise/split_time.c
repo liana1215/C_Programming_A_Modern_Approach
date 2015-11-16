269b648fb72b2f4d31e1343e108e0cd460ad1379
tasuku <tasuku@gmail.com>
2015-11-16 22:56:05 +0900

    added comments, and formatted.

--- ch16/exercise/split_time.c
+++ ch16/exercise/split_time.c
@@ -1,38 +1,40 @@
+/*Section 16.2 Exercise 6*/
 #include <stdio.h>
 
-
-
 struct time {
-    int hours;
-    int minutes;
-    int seconds;
+    unsigned int hours;
+    unsigned int minutes;
+    unsigned int seconds;
 };
 
 struct time split_time(long total_seconds);
 
 int main(int argc, char *argv[]){
     
-    struct time split_t;
+    struct time split;
     int s = 0;
 
     printf("input the number seconds: ");
     scanf("%d",&s);
     
-    split_t = split_time(s);
+    split = split_time(s);
 
-    printf("hours: %d\n",split_t.hours);
-    printf("minutes: %d\n",split_t.minutes);
-    printf("seconds: %d\n",split_t.seconds);
+    printf("hours: %d\n", split.hours);
+    printf("minutes: %d\n", split.minutes);
+    printf("seconds: %d\n", split.seconds);
 
     return 0;
 }
 
+/*takes in total seconds, and returns a struct with structure variables
+ * hours, minutes, seconds*/
 struct time split_time(long total_seconds){
-    struct time split_t;
-    split_t.hours = total_seconds/(3600);
-    split_t.minutes = (total_seconds - split_t.hours*3600)/60;
-    split_t.seconds = total_seconds - split_t.hours * 3600 - split_t.minutes * 60;
+    struct time split;
+
+    split.hours = total_seconds / 3600;
+    split.minutes = (total_seconds - split.hours * 3600) / 60;
+    split.seconds = total_seconds - split.hours * 3600 - split.minutes * 60;
     
-    return split_t;
+    return split;
 }
 
