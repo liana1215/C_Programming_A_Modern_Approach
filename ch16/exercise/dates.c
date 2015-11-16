c8a9d9aa1522da417f770e68c2c11c80527b5545
tasuku <tasuku@gmail.com>
2015-11-16 21:50:52 +0900

    retab

--- ch16/exercise/dates.c
+++ ch16/exercise/dates.c
@@ -2,9 +2,9 @@
 
 
 struct date {
-	int month;
-	int day;
-	int year;
+    int month;
+    int day;
+    int year;
 };
 
 
@@ -14,36 +14,36 @@ int compare_dates(struct date d1, struct date d2);
 
 
 int main (int argc, char* argv[]){
-	struct date d;
-	int days;
+    struct date d;
+    int days;
 
-	d.month = 2;
-	d.day 	= 5;
-	d.year  = 2015;
+    d.month = 2;
+    d.day   = 5;
+    d.year  = 2015;
 
-	days = day_of_year(d);
-	printf("The number of days is: %d\n",days);
-			
-	return 0;
+    days = day_of_year(d);
+    printf("The number of days is: %d\n",days);
+            
+    return 0;
 }
 
 int day_of_year(struct date d){
-	int i, m_total = 0;
-	int arr[12] = {31,28,31,30,31,30,31,31,30,31,30,31,};
+    int i, m_total = 0;
+    int arr[12] = {31,28,31,30,31,30,31,31,30,31,30,31,};
 
-	for (i = 0; i < d.month; i++)
-		m_total = m_total + arr[i];
+    for (i = 0; i < d.month; i++)
+        m_total = m_total + arr[i];
 
-	return m_total + d.day;
+    return m_total + d.day;
 
 }
 
 int compare_dates(struct date d1, struct date d2){
-	
-	int ret_val = 0;	
+    
+    int ret_val = 0;    
 
-	if (d1.year <= d2.year){
-		ret_val = -1;
-	else
+    if (d1.year <= d2.year){
+        ret_val = -1;
+    else
 }
 
--- ch16/exercise/split_time.c
+++ ch16/exercise/split_time.c
@@ -3,36 +3,36 @@
 
 
 struct time {
-	int hours;
-	int minutes;
-	int seconds;
+    int hours;
+    int minutes;
+    int seconds;
 };
 
 struct time split_time(long total_seconds);
 
 int main(int argc, char *argv[]){
-	
-	struct time split_t;
-	int s = 0;
+    
+    struct time split_t;
+    int s = 0;
 
-	printf("input the number seconds: ");
-	scanf("%d",&s);
-	
-	split_t = split_time(s);
+    printf("input the number seconds: ");
+    scanf("%d",&s);
+    
+    split_t = split_time(s);
 
-	printf("hours: %d\n",split_t.hours);
-	printf("minutes: %d\n",split_t.minutes);
-	printf("seconds: %d\n",split_t.seconds);
+    printf("hours: %d\n",split_t.hours);
+    printf("minutes: %d\n",split_t.minutes);
+    printf("seconds: %d\n",split_t.seconds);
 
-	return 0;
+    return 0;
 }
 
 struct time split_time(long total_seconds){
-	struct time split_t;
-	split_t.hours = total_seconds/(3600);
-	split_t.minutes = (total_seconds - split_t.hours*3600)/60;
-	split_t.seconds = total_seconds - split_t.hours * 3600 - split_t.minutes * 60;
-	
-	return split_t;
+    struct time split_t;
+    split_t.hours = total_seconds/(3600);
+    split_t.minutes = (total_seconds - split_t.hours*3600)/60;
+    split_t.seconds = total_seconds - split_t.hours * 3600 - split_t.minutes * 60;
+    
+    return split_t;
 }
 
