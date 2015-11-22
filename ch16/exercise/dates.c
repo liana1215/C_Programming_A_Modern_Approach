4862ad9df8e2a966c360542790a43a7dc79a4327
tasuku <tasuku@gmail.com>
2015-11-22 18:16:57 +0900

    edited types

--- ch16/exercise/dates.c
+++ ch16/exercise/dates.c
@@ -8,21 +8,21 @@ struct date {
 };
 
 
-int day_of_year(struct date d);
-int compare_dates(struct date d1, struct date d2);
+static unsigned int day_of_year(struct date d);
+static int compare_dates(struct date d1, struct date d2);
 
-int main(int argc, char* argv[]){
-    
+int 
+main(int argc, char* argv[]) {
     struct date d1,d2;
-    int days;
+    unsigned int days;
 
-    d1.month = 2;
+    d1.month = 0;
     d1.day   = 5;
     d1.year  = 1990;
     
-    d2.month = 5;
-    d2.day   = 10;
-    d2.year  = 1999;
+    d2.month = 2;
+    d2.day   = 4;
+    d2.year  = 1990;
     
     days = day_of_year(d1);
     printf("The number of days is: %d\n",days);
@@ -33,10 +33,10 @@ compare_dates(d1,d2));
 }
  
 /*Returns the day of the year (an integer between 1 and 366) that corresponds to the date d.*/
-int day_of_year(struct date d){
-    
+static unsigned int 
+day_of_year(struct date d) {
     int i, m_total = 0;
-    int arr[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    unsigned int arr[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
 
     for (i = 0; i < d.month; i++)
         m_total = m_total + arr[i];
@@ -46,12 +46,12 @@ int day_of_year(struct date d){
 
 /*Returns -1 if d1 is an earlier date than d2, +1 if d1 is a later date than d2,
  * and 0 if d1 and d2 are the same*/
-int compare_dates(struct date d1, struct date d2){
-    
-    int d1_dofy = day_of_year(d1);
-    int d2_dofy = day_of_year(d2);
+static int 
+compare_dates(struct date d1, struct date d2) {    
+    unsigned int d1_dofy = day_of_year(d1);
+    unsigned int d2_dofy = day_of_year(d2);
 
-    if (d1.year == d2.year){
+    if (d1.year == d2.year) {
         if (d1_dofy == d2_dofy)
             return 0;
         else if (d1_dofy > d2_dofy)
@@ -63,6 +63,5 @@ int compare_dates(struct date d1, struct date d2){
         return 1;
     else
         return -1;
-    
 }
     
