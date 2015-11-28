dde4020d08440a875c0c05ca105af6b31cb24c80
tasuku <tasuku@gmail.com>
2015-11-28 21:40:50 +0900

    reformatted structure, and edited do-while loop

--- ch16/exercise/16_p1_countrycode.c
+++ ch16/exercise/16_p1_countrycode.c
@@ -7,38 +7,42 @@ struct dialing_code {
     int code;
 };
 
-int main (int argc, char* argv[]) {
-    const struct dialing_code country_codes[] = 
-    {{"Argentina",  54}, {"Bangladesh",        880},
-    {"Brazil",     55}, {"Burma (Myanmar)",    95},
-    {"China",      86}, {"Colombia",           57},
-    {"Congo,Dem.",243}, {"Egypt",              20},
-    {"Ethiopia",  251}, {"France",             33},
-    {"Germany",    49}, {"India",              91},
-    {"Indonesia",  62}, {"Iran",               98},
-    {"Italy",      39}, {"Japan",              81},
-    {"Mexico",     52}, {"Nigeria",           234},
-    {"Pakistan",   92}, {"Philippines",        63},
-    {"Poland",     48}, {"Russia",              7}};
+int 
+main (int argc, char* argv[]) {
+    int intl_code, i;
+    const struct dialing_code country_codes[] = {
+        {"Argentina",  54}, {"Bangladesh",        880},
+        {"Brazil",     55}, {"Burma (Myanmar)",    95},
+        {"China",      86}, {"Colombia",           57},
+        {"Congo,Dem.",243}, {"Egypt",              20},
+        {"Ethiopia",  251}, {"France",             33},
+        {"Germany",    49}, {"India",              91},
+        {"Indonesia",  62}, {"Iran",               98},
+        {"Italy",      39}, {"Japan",              81},
+        {"Mexico",     52}, {"Nigeria",           234},
+        {"Pakistan",   92}, {"Philippines",        63},
+        {"Poland",     48}, {"Russia",              7}
+        };
 
-    int int_code, i;
-    int array_length = sizeof(country_codes)/sizeof(*country_codes);
+    int n_entries = sizeof(country_codes) / sizeof(*country_codes);
    
     do {
-        int flag = 0;
+        int found = 0;
 
         printf("Please input the international code(-1 to exit): ");
-        scanf("%d",&int_code);
+        scanf("%d", &intl_code);
+        if (intl_code == -1)
+            break;
     
-        for (i = 0; i < array_length; i++) {
-            if (country_codes[i].code == int_code) {
+        for (i = 0; i < n_entries; i++) {
+            if (country_codes[i].code == intl_code) {
                 printf("The country is: %s\n", country_codes[i].country);
-                flag = 1;
+                found = 1;
             }
         }
-        if (flag == 0) 
+        if (!found) 
             printf("Code not found.\n");
-    } while (int_code >= 0);
+    } while(1);
 
     return 0;
 }
